b851bca6f371d6a533c711543df6597d17dddac5
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-03-15 22:06:38 -0600

    Iniciando
    Form en index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GYM</title>
+  </head>
+  <body>
+    <form id="frminformacion"><label> Nombre: </label>
+ <input type="text" name="nombre" />
+
+ <label> Apellidos: </label>
+ <input type="text" name="apellidos" />
+
+ <label> Sexo: </label>
+ <input type="radio" name="sexo" value="f" /> Femenino
+ <input type="radio" name="sexo" value="m" /> Masculino
+
+ <label> Grado: </label>
+<select name="grado"> <option value="1"> Primero </option></select>
+<select name="grado"> <option value="2"> Segundo </option></select>
+<select name="grado"> <option value="2"> Tercero </option></select>
+
+ <input id="enviar" type="button" value="Enviar" /></form>
+  </body>
+</html>
