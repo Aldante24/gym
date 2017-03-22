529bdffd7d3b60fc3f218c0315fa9d1f2f73cdf0
Aldo Martinez Amador <mtz.aldo24@gmail.com>
2017-03-22 00:09:31 -0600

    jQuery
    Se hizo independiente del form el codigo de envio de datos con jQuery

--- vista/js/script.js
+++ vista/js/script.js
@@ -1,17 +1,23 @@
-    function enviarDatos(){
-        $("#frm").on("submit", function(e){
-          e.preventDefault();
-          var nombre_ = $("#nombre").val();
-          var sexo_ = $("#sexo").val();
-          var dir_ = $("#direccion").val();
-          var email_ = $("#email").val();
-          var tel_ = $("#telefono").val();
-          var fin_ = $("#fechain").val();
-          var img_ = $("#imagen").val();
-          var puesto_ = $("#puesto").val();
-          $.post("vista/guardar.php", {nombre:nombre_, sexo:sexo_, direccion:dir_, email:email_, telefono:tel_, fechain:fin_, imagen:img_, puesto:puesto_},
-          function(datos){
-            $("#resultado").html(datos)
-              });
+$(document).on("ready",function(){
+  enviarDatos();
+});
+
+function enviarDatos(){
+  $("#frm").on("submit", function(e){
+    e.preventDefault();
+    var nombre_ = $("#nombre").val();
+    var sexo_ = $("#sexo").val();
+    var dir_ = $("#direccion").val();
+    var email_ = $("#email").val();
+    var tel_ = $("#telefono").val();
+    var ffin_ = $("#fechafin").val();
+    var fin_ = $("#fechain").val();
+    var img_ = $("#imagen").val();
+    var membresia_ = $("#membresia").val();
+
+    $.post("guardar.php", {nombre:nombre_, sexo:sexo_, direccion:dir_, email:email_, telefono:tel_, fechafin:ffin_, fechain:fin_, imagen:img_, membresia:membresia_},
+    function(datos){
+      $("#resultado").html(datos)
         });
-      };
+  });
+};
--- vista/registro.php
+++ vista/registro.php
@@ -6,33 +6,7 @@
   <head>
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-    <!-- <script src="vista/js/script.js"></script> -->
-    <script>
-      $(document).on("ready",function(){
-        enviarDatos();
-      });
-
-      function enviarDatos(){
-        $("#frm").on("submit", function(e){
-          e.preventDefault();
-          var nombre_ = $("#nombre").val();
-          var sexo_ = $("#sexo").val();
-          var dir_ = $("#direccion").val();
-          var email_ = $("#email").val();
-          var tel_ = $("#telefono").val();
-          var ffin_ = $("#fechafin").val();
-          var fin_ = $("#fechain").val();
-          var img_ = $("#imagen").val();
-          var membresia_ = $("#membresia").val();
-
-          $.post("guardar.php", {nombre:nombre_, sexo:sexo_, direccion:dir_, email:email_, telefono:tel_, fechafin:ffin_, fechain:fin_, imagen:img_, membresia:membresia_},
-          function(datos){
-            $("#resultado").html(datos)
-              });
-        });
-      };
-
-    </script>
+    <script src="js/script.js"></script>
     <title>GYM</title>
     <section class="container">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
